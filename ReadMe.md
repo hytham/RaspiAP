b525c575c6789680118d2955545537e44e690c12
Hytham Alobydi <haiwa80@biomedicallc.net>
2020-08-27 15:08:02 -0400

    first checkin

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+## RASPIAP
+
+This tool will setup raspberypi as an Access point, based on this URL 
+https://www.raspberrypi.org/documentation/configuration/wireless/access-point-routed.md
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Installing required softwares ....."
+sudo apt install hostapd
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+
+sudo apt install dnsmasq
+sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
+
+cp routed-ap.conf /etc/sysctl.d/routed-ap.conf
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo netfilter-persistent save
+
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo cp dnsmasq.conf  /etc/dnsmasq.conf
+
+echo "Check if rf is compatable"
+
+sudo rfkill unblock wlan
+
+sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+
+
