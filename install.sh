3fe68e9f02cc9129b492ef911acf531a7f7a0621
Hytham Alobydi <haiwa80@biomedicallc.net>
2020-08-27 15:53:58 -0400

    add the wlan diff

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ sudo apt -y update
 sudo apt -y upgrade
 
 echo "Installing required softwares ....."
-sudo apt install hostapd
+sudo apt install -y hostapd
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
 
-sudo apt install dnsmasq
+sudo apt install -y dnsmasq
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 
 cp routed-ap.conf /etc/sysctl.d/routed-ap.conf
@@ -19,10 +19,18 @@ sudo netfilter-persistent save
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 sudo cp dnsmasq.conf  /etc/dnsmasq.conf
 
+echo "interface wlan0" >> /etc/dhcpcd.conf
+echo "    static ip_address=192.168.4.1/24"  >> /etc/dhcpcd.conf
+echo "   nohook wpa_supplicant"  >> /etc/dhcpcd.conf
+
 echo "Check if rf is compatable"
 
 sudo rfkill unblock wlan
 
+echo "Setting up the access point"
 sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+
+echo "Rebooting"
+sudo systemctl reboot
 
 
